148cbb74bc89ff85335682d9c4b6929d64154ae6
MalkaGit <regine.issan.follow@gmail.com>
2026-02-01 20:10:59 +0200

    feature/uc00004B-backend-add-like-operation--post-me-likes: fix file names

--- serverWorkspace/app-rest-api/src/modules/likes/types/index.ts
+++ serverWorkspace/app-rest-api/src/modules/likes/types/index.ts
@@ -6,9 +6,9 @@
  */
 
 export { LikedEntityType, LIKED_ENTITY_TYPES } from "./liked.entity.type";
-export { AddLikeInput } from "./likes.add.like.input.model";
-export { AddLikeOutput } from "./likes.add.like.output";
-export { AddLikeInputSchema } from "./likes.add.like.input.schema";
+export { AddLikeInput } from "./add.like.input.model";
+export { AddLikeOutput } from "./add.like.output";
+export { AddLikeInputSchema } from "./add.like.input.schema";
 export { LikesItem  } from "./likes.item.model";
 export { QueryLikesInput } from "./query.likes.input.model";
 export { QueryLikesInputSchema } from "./query.likes.input.schema";
